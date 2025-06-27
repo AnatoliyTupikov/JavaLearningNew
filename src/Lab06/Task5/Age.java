d677407991fe8d113ebd9c7abf5b8e612d25069c
Anatolii Tupikov <atupikov775@gmail.com>
2025-06-27 17:43:53 +0200

    Lab06. Fields were removed

--- src/Lab06/Task5/Age.java
+++ src/Lab06/Task5/Age.java
@@ -3,16 +3,16 @@ package Lab06.Task5;
 import java.util.Scanner;
 
 public class Age {
-    public int Age;
+    public Scanner scnr = new Scanner(System.in);
 
     public String consoleInput(String label) {
-        var scanner = new Scanner(System.in);
+
         System.out.print(label + " ");
-        return scanner.nextLine();
+        return scnr.nextLine();
     }
 
     public void set() {
-        this.Age = Integer.getInteger(this.consoleInput("Введите возраст: "));
-        System.out.println("Возраст: " + Age);
+        int age = Integer.getInteger(this.consoleInput("Введите возраст: "));
+        System.out.println("Возраст: " + age);
     }
 }
--- src/Lab06/Task5/Name.java
+++ src/Lab06/Task5/Name.java
@@ -1,11 +1,11 @@
 package Lab06.Task5;
 
 public class Name extends Age {
-    public String userName;
+
 
     @Override
     public void set() {
-        this.userName = super.consoleInput("Введите имя: ");
+       String userName = super.consoleInput("Введите имя: ");
         System.out.println("Имя: " + userName);
     }
 
