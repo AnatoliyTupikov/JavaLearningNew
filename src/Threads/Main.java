cef40e53474181d64ce0b0c470a15d0c2fb40b10
Anatolii Tupikov <atupikov775@gmail.com>
2025-07-08 19:39:13 +0200

    Threads lab. Init

---
+++ src/Threads/Main.java
@@ -0,0 +1,15 @@
+package Threads;
+
+import Lab09.Game;
+import Lab09.SimpleList;
+import Lab09.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] ars) {
+
+    }
+}
