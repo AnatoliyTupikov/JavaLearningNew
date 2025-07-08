2b46dc3aeff9028f5cf0c34608e6c6fdb129165f
Anatolii Tupikov <atupikov775@gmail.com>
2025-07-08 18:37:14 +0200

    Lab10. Init!

---
+++ src/Lab10/Main.java
@@ -0,0 +1,17 @@
+package Lab10;
+
+import Lab09.Game;
+import Lab09.SimpleList;
+import Lab09.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] ars) {
+
+
+
+    }
+}
